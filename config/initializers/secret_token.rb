fab8a9333261f12fdb525830f6a77837931c4a22
Tim Jakobsson <tim.jakobsson@gmail.com>
2011-01-11 18:54:01 +0100

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MynewsdeskApi::Application.config.secret_token = '3388975174490bad02e3901023df0e1acd84eb16ed83387f028beca03889bad836080ca66f35562e1c03815b39ebadf1e9d05cb53fdcc85e7947ebdfd0a6c172'
