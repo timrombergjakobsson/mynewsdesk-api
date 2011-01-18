32c7abe80b12ff8e5e2e27f69d00f19e21ec99f4
Tim Jakobsson <tim.jakobsson@gmail.com>
2011-01-18 10:46:25 +0100

    removed some code

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-   console.log("hello")
    $('#content nav ul').infinitescroll({
       navSelector:   "nav#pagination",
       nextSelector:  "nav#pagination #next",
       itemSelector:  "#content li",
       debug:         true,
-      loadingText:   "Loading more news",
+      loadingText:   "Loading more news	",
       loadingImg:    "/images/loading.gif",
       animate:       true,
       bufferPx:      10
